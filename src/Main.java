35947100b2d1a377ab5fa9a9cffcd712bb5e8386
SnehalMarutiSalokhe <snehal.maruti.salokhe@gmail.com>
2024-11-29 16:13:59 +0530

    Added :  Ability to Displaying Contact

--- src/Main.java
+++ src/Main.java
@@ -13,13 +13,17 @@ public class Main {
 
         int isExit = 1;
         do {
-            System.out.println("Enter 1 to create contact ");
+            System.out.println("Enter 1 to create contact \n Enter 2 to display al contacts");
             int choice  = sc.nextInt();
             switch (choice) {
                 case 1 -> createContact(a1);
 
+                case 2 ->  a1.display();
+
                 default -> System.out.println("Wrong input");
             }
+
+
             System.out.println("Enter 0 to exit");
             isExit = sc.nextInt();
             System.out.println("Exiting from addressBook ..");

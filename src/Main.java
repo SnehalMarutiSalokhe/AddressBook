71384cd8ea57930d0813d75f1d90cda3e0edb21f
SnehalMarutiSalokhe <snehal.maruti.salokhe@gmail.com>
2024-11-29 22:14:18 +0530

    Added : Ability to Edit Existing Contact

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Welcome to Address Book!");
 
         AddressBook a1 = new AddressBook();
@@ -13,47 +9,49 @@ public class Main {
 
         int isExit = 1;
         do {
-            System.out.println("Enter 1 to create contact \n Enter 2 to display al contacts");
-            int choice  = sc.nextInt();
+            System.out.println("""
+                Enter 1 to create contact
+                Enter 2 to display all contacts
+                Enter 3 to edit an existing contact""");
+            int choice = sc.nextInt();
             switch (choice) {
                 case 1 -> createContact(a1);
-
-                case 2 ->  a1.display();
-
+                case 2 -> a1.display();
+                case 3 -> {
+                    System.out.println("Enter the first name of the contact to edit:");
+                    String firstName = sc.next();
+                    a1.editContact(firstName);
+                }
                 default -> System.out.println("Wrong input");
             }
 
-
             System.out.println("Enter 0 to exit");
             isExit = sc.nextInt();
-            System.out.println("Exiting from addressBook ..");
 
         } while (isExit != 0);
 
-        System.out.println("Thank you for using address book");
+        System.out.println("Exiting from Address Book...");
+        System.out.println("Thank you for using Address Book.");
         sc.close();
-
-
     }
 
     static void createContact(AddressBook a1) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter name");
-        String name = sc.next();
-        System.out.println("Enter last name");
-        String lname = sc.next();
-        System.out.println("Enter city");
+        System.out.println("Enter first name:");
+        String firstName = sc.next();
+        System.out.println("Enter last name:");
+        String lastName = sc.next();
+        System.out.println("Enter city:");
         String city = sc.next();
-        System.out.println("Enter state");
+        System.out.println("Enter state:");
         String state = sc.next();
-        System.out.println("Enter email");
+        System.out.println("Enter email:");
         String email = sc.next();
-        System.out.println("Enter phone");
+        System.out.println("Enter phone number:");
         int phone = sc.nextInt();
-        System.out.println("Enter zip");
+        System.out.println("Enter zip code:");
         int zip = sc.nextInt();
-        Contact c1 = new Contact(name, lname, city, state, email, phone, zip);
+        Contact c1 = new Contact(firstName, lastName, city, state, email, phone, zip);
         a1.addContact(c1);
-
     }
 }
